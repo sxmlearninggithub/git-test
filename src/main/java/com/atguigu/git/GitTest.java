21d3c323190b743ad4e860a410a3c8d877836090
sxm <sxm@qq.com>
2022-04-15 09:30:45 +0800

    hot-fix test 2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git hub");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
-        System.out.println("hot-fix test!");
+        System.out.println("hot-fix test 1!");
+        System.out.println("hot-fix test 2!");
     }
 }
