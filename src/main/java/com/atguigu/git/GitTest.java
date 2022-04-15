ed8bde094fd92f3449a00b2451fd31769b67725f
sxm <sxm@qq.com>
2022-04-15 15:55:15 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master test 2!");
         System.out.println("hot-fix test 1!");
         System.out.println("hot-fix test 2!");
+        System.out.println("put test!");
     }
 }
